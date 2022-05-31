311f5615d4b392ee85b592d14a5a39df471bd67b
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-05-31 18:29:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.56%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+     int arr[n+1];
+        arr[0]=1,arr[1]=1;
+        for(int i=2;i<=n;i++)
+            arr[i]=arr[i-2]+arr[i-1];
+        return arr[n];
+    }
+};
