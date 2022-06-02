550e0368cea63b95989daa5745082c8d8ace4b31
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-02 10:22:53 +0530

    Time: 469 ms (45.33%), Space: 0B (100.00%) - LeetHub

---
+++ 608-tree-node/608-tree-node.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT id,'Root' AS type FROM Tree WHERE
+p_id IS NULL
+UNION
+
+SELECT id,'Inner' AS type FROM Tree WHERE 
+id IN(SELECT p_id FROM Tree)
+AND p_id IS NOT NULL
+UNION
+
+SELECT id,'Leaf' AS type FROM Tree WHERE
+id NOT IN(SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
+AND p_id IS NOT NUll;
