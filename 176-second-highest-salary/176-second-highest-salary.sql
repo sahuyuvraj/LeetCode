9fb6a28a10cbf2bfc52040a1fd053e0ce2c06aa3
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-02 10:37:25 +0530

    Time: 250 ms (45.51%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,2 +1,10 @@
 # Write your MySQL query statement below
-SELECT MAX(salary) as SecondHighestSalary FROM Employee WHERE salary <>(SELECT MAX(salary) FROM Employee);
+#SELECT MAX(salary) as SecondHighestSalary FROM Employee WHERE salary <>(SELECT MAX(salary) FROM Employee);
+SELECT MAX(salary) AS SecondHighestSalary 
+FROM Employee 
+WHERE salary <> (
+    SELECT salary 
+    FROM Employee 
+    GROUP BY salary 
+    ORDER BY salary 
+    DESC LIMIT 1);
