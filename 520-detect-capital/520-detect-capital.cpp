1cbb6984f0be2bb8766d7f3e57469e6fba878ee4
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-07 16:14:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (90.47%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size()==1)return true;
+
+        if(isupper(word[0])){
+            if(isupper(word[1])){
+                for(int i=2;i<word.size();i++)
+                    if(!isupper(word[i]))return false;
+            }else{
+                for(int i=1;i<word.size();i++)
+                    if(isupper(word[i]))return false;
+            }
+        }else{
+            for(int i=1;i<word.size();i++)
+                if(isupper(word[i]))return false;
+        }
+        return true;
+    }
+};
