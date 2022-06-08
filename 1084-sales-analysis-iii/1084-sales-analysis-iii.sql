aebe81c7a6661a40355fab973dda1e453b135263
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-08 23:30:11 +0530

    Time: 1895 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select product_id, product_name from product where product_id not in
+(select product_id from sales where sale_date<"2019-01-01" or sale_date>"2019-03-31")
