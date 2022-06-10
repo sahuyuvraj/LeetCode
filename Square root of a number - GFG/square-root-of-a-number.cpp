56b2dffd2d11728dadd81a7271d7643d6c2c95e8
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-10 22:20:39 +0530

    Added solution - LeetHub

--- Square root of a number - GFG/square-root-of-a-number.cpp
+++ Square root of a number - GFG/square-root-of-a-number.cpp
@@ -12,7 +12,13 @@ class Solution{
     long long int floorSqrt(long long int x) 
     {
         // Your code goes here  
-        return sqrt(x);
+        long long low=1,high=x,mid,ans=1;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(mid*mid<=x)ans=mid,low=mid+1;
+            else high=mid-1;
+        }
+        return ans;
     }
 };
 
