e0c220ecfbe0abf2c85565bd580f01c14f923c82
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-22 15:00:31 +0530

    Added solution - LeetHub

---
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	int minCoins(int coins[], int M, int V) 
+	{ 
+	    // Your code goes here
+	    int dp[M+1][V+1];
+	    for(int j=0;j<=V;j++)dp[0][j]=INT_MAX-1;
+	    for(int i=0;i<=M;i++)dp[i][0]=0;
+	    
+	    for(int i=1;i<=M;i++){
+	        for(int j=1;j<=V;j++){
+	            if(coins[i-1]>j)dp[i][j]=dp[i-1][j];
+	            else dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+	        }
+	    }
+	    return dp[M][V]==INT_MAX-1?-1:dp[M][V];
+	} 
+	  
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int v, m;
+        cin >> v >> m;
+
+        int coins[m];
+        for(int i = 0; i < m; i++)
+        	cin >> coins[i];
+
+      
+	    Solution ob;
+	    cout << ob.minCoins(coins, m, v) << "\n";
+	     
+    }
+    return 0;
+}
+  // } Driver Code Ends
