5ed734a740149b70a79ed0d311f66cdb13b20df2
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-24 16:37:00 +0530

    Time: 76 ms (36.36%), Space: 28.6 MB (91.61%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        priority_queue <int> pq;
+        long sum=0,tmp;
+        for(int i=0;i<target.size();++i){
+            sum+=target[i];
+            if(target[i]!=1)pq.push(target[i]);
+        }
+        while(!pq.empty()&&pq.top()!=1){
+            tmp=pq.top();
+            pq.pop();
+            sum-=tmp;
+            if(sum==0||tmp<=sum)return 0;
+            tmp%=sum;
+            sum+=tmp;
+            if(tmp==1)continue;
+            pq.push(tmp?tmp:sum);
+        }
+        return 1;
+    }
+};
