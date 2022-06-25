7dd8ad8e7cca99971a963e7782e4494b5d26afda
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-25 19:59:10 +0530

    Added solution - LeetHub

--- Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
@@ -10,21 +10,35 @@ using namespace std;
 int maxProfit(vector<int>&price){
     //Write your code here..
     int n=price.size();
-    int dp[n];
-    dp[n-1]=0;
-    int ma=price[n-1];
-    for(int i=n-2;i>=0;i--){
-        dp[i]= max(dp[i+1],ma-price[i]);
-        if(ma<price[i])ma=price[i];
-    }
+    int buy1=price[0];
+    int buy2=price[0];
+    int profit1=0;
+    int profit2=0;
     
-    int mi=price[0];
-    for(int i=1;i<n;i++){
-        dp[i]=max(dp[i-1],dp[i]+price[i]-mi);
-        if(mi>price[i])mi=price[i];
+    for(int i=0;i<n;i++){
+        buy1=min(buy1,price[i]);
+        profit1=max(profit1,price[i]-buy1);
+        
+        buy2=min(buy2,price[i]-profit1);
+        profit2=max(profit2,price[i]-buy2);
     }
+    return profit2;
+    // int n=price.size();
+    // int dp[n];
+    // dp[n-1]=0;
+    // int ma=price[n-1];
+    // for(int i=n-2;i>=0;i--){
+    //     dp[i]= max(dp[i+1],ma-price[i]);
+    //     if(ma<price[i])ma=price[i];
+    // }
     
-    return dp[n-1];
+    // int mi=price[0];
+    // for(int i=1;i<n;i++){
+    //     dp[i]=max(dp[i-1],dp[i]+price[i]-mi);
+    //     if(mi>price[i])mi=price[i];
+    // }
+    
+    // return dp[n-1];
 }
 
 // { Driver Code Starts.
