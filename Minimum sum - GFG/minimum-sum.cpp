0081901dff05fb576616d1e3b35ba4c155358f8f
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-06-27 22:35:48 +0530

    Added solution - LeetHub

--- Minimum sum - GFG/minimum-sum.cpp
+++ Minimum sum - GFG/minimum-sum.cpp
@@ -9,42 +9,62 @@ using namespace std;
 
 class Solution{   
 public:
-      
-      string sumstr(string s1,string s2){
-          int n1=s1.size(),n2=s2.size();
-          int i=n1-1;
-          int j=n2-1;
-          
-          string ans="";
-          
-          int c=0;
-          
-          while(i>=0 or j>=0 or c){
-              
-              int sum=0;
-              if(i>=0)sum+=s1[i--]-'0';
-              if(j>=0)sum+=s2[j--]-'0';
-              if(c)sum+=c;
-              ans+=to_string(sum%10);
-              c=sum/10;
-          }
-          reverse(ans.begin(),ans.end());
-          i=0;
-          while(ans[i]=='0')i++;
-          return ans.substr(i);
-      }
-       
+
+    // string sum(string s1,string s2){
+    //     int n1=s1.size();
+    //     int n2=s2.size();
+        
+    // }
+    
+    
     string solve(int arr[], int n) {
         // code here
+        
         sort(arr,arr+n);
-        string s1="",s2="";
-        for(int i=0;i<n;i++){
-            s1+=to_string(arr[i++]);
-            if(i<n)s2+=to_string(arr[i]);
-        }
-        //cout<<s1<<endl<<s2<<endl;
-        string sum=sumstr(s1,s2);
-        return sum;
+        // string s1="";
+        // string s2="";
+        string ans="";
+         int end=n-1;
+         int c=0;
+         while(end>=0){
+
+          int sum=0;
+          sum+=arr[end--];
+          if(end>=0)sum+=arr[end--];
+          if(c!=0)sum+=c;
+          ans+=to_string(sum%10);
+          c=sum/10;
+         }
+         if(c!=0)ans+=to_string(c);
+        reverse(ans.begin(),ans.end());
+        int i=0;
+        while(ans[i]=='0')i++;
+        //string ans=sum(s1,s2);
+        
+        return ans.substr(i);
+        
+        
+        
+// N = 6
+// Arr[] = {6, 8, 4, 5, 2, 3}
+//          2, 3, 4, 5, 6 ,8
+//          2     4     6
+//           3      5     8
+           
+           
+// Output: 604
+// Explanation: The minimum sum is formed 
+// by numbers 358 and 246.
+
+// N = 5
+// Arr[] = {5, 3, 0, 7, 4}
+//          0, 3, 4, 5, 7
+//          0    4     7
+//           3     5
+           
+// Output: 82
+// Explanation: The minimum sum is 
+// formed by numbers 35 and 047.
     }
 };
 
