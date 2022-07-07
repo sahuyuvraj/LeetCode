e3f7f5ad38e392af77f9a8a13cb0ab5dcba79da7
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-07 12:28:34 +0530

    Added solution - LeetHub

---
+++ Interleaved Strings - GFG/interleaved-strings.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     int dp[101][101];
+    bool solve(string s1,int n,string s2,int m,string s3,int len){
+        if(len==0)return 1;
+        if(dp[n][m]!=-1)return dp[n][m];
+        
+        int a=0,b=0;
+        if(n-1>=0 and s1[n-1]==s3[len-1])a=solve(s1,n-1,s2,m,s3,len-1);
+        if(m-1>=0 and s2[m-1]==s3[len-1])b=solve(s1,n,s2,m-1,s3,len-1);
+        
+        return dp[n][m]=a or b;
+    }
+    
+    /*You are required to complete this method */
+    
+    bool isInterleave(string s1, string s2, string s3) 
+    {
+        //Your code here
+        int n=s1.size();
+        int m=s2.size();
+        int len=s3.size();
+        if(n+m!=len)return false;
+        dp[n][m];
+        memset(dp,-1,sizeof(dp));
+        return solve(s1,n,s2,m,s3,len);
+    }
+
+};
+
+// { Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string a,b,c;
+		cin>>a;
+		cin>>b;
+		cin>>c;
+		Solution obj;
+		cout<<obj.isInterleave(a,b,c)<<endl;
+	}
+	// your code goes here
+	return 0;
+}  // } Driver Code Ends
