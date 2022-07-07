e42b4c5ce8055ee59309b64f5ad985b4b59c4cb2
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-07 23:58:57 +0530

    Added solution - LeetHub

--- Shop in Candy Store - GFG/shop-in-candy-store.cpp
+++ Shop in Candy Store - GFG/shop-in-candy-store.cpp
@@ -9,27 +9,42 @@ public:
     vector<int> candyStore(int candies[], int N, int K)
     {
         // Write Your Code here
-        sort(candies,candies+N);
+    //   sort(candies,candies+N);
         
-        int mn=0,mx=0;
-        int i=0,j=N-1;
-        while(i<=j){
-            mn+=candies[i++];
-            int k=K;
-            while(k--){
-                j--;
-            }
-        }
+    //     int mn=0,mx=0;
+    //     int i=0,j=N-1;
+    //     while(i<=j){
+    //         mn+=candies[i++];
+    //         int k=K;
+    //         while(k--){
+    //             j--;
+    //         }
+    //     }
         
-         i=0,j=N-1;
-        while(i<=j){
-            mx+=candies[j--];
-            int k=K;
-            while(k--){
-                i++;
-            }
+    //      i=0,j=N-1;
+    //     while(i<=j){
+    //         mx+=candies[j--];
+    //         int k=K;
+    //         while(k--){
+    //             i++;
+    //         }
+    //     }
+    //   return {mn,mx};        
+         
+         
+        int n = N;
+        int k = K;
+        sort(candies,candies+n);
+        int x = (n+k)/(k+1);
+        int ans1 = 0;
+        for(int i=0;i<x;i++){
+            ans1 += candies[i];
         }
-      return {mn,mx};        
+        int ans2 = 0;
+        for(int i=n-1;i>n-1-x;i--){
+            ans2 += candies[i];
+        }
+        return {ans1,ans2};
     }
 };
 
