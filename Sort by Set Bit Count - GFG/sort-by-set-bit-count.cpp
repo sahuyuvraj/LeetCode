73131c5a0295781398a544627c99e3e2d482105f
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-15 09:16:08 +0530

    Added solution - LeetHub

--- Sort by Set Bit Count - GFG/sort-by-set-bit-count.cpp
+++ Sort by Set Bit Count - GFG/sort-by-set-bit-count.cpp
@@ -4,39 +4,19 @@ using namespace std;
 
 
  // } Driver Code Ends
+
+   static bool cmp(int a, int b){
+      return __builtin_popcount(a)>__builtin_popcount(b);
+  }
 class Solution{
     public:
-    int count_bit(int n){
-        int cnt=0;
-        while(n){
-            if(n&1)cnt++;
-            n>>=1;
-        }
-        return cnt;
-    }
-    static bool cmp(pair<int,int>&a,pair<int,int>&b){
-        if(a.first==b.first)
-        return a.second<b.second;
-        return a.first>b.first;
-    }
+    
     void sortBySetBitCount(int arr[], int n)
     {
         // Your code goes here
-        vector<pair<int,int>>v;
-        int arr2[n];
-        for(int i=0;i<n;i++)arr2[i]=arr[i];
-       for(int i=0;i<n;i++){
-           int cnt=count_bit(arr[i]);
-           v.push_back({cnt,i});
-       }
-       sort(v.begin(),v.end(),cmp);
-       for(int i=0;i<n;i++){
-           arr[i]=arr2[v[i].second];
-       }
+        stable_sort(arr,arr+n,cmp);
     }
 };
-
-
 
 // { Driver Code Starts.
 
