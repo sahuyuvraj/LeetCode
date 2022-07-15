67103eebe6730f19893fc1d44238988f9195317a
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-16 00:56:22 +0530

    Added solution - LeetHub

---
+++ Majority Element - GFG/majority-element.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+        if(size==1)return a[0];
+        sort(a,a+size);
+        int cnt=1;
+        for(int i=1;i<size;i++){
+            if(a[i-1]==a[i]){
+                cnt++;
+                if(cnt>size/2)return a[i];
+            }else{
+                cnt=1;
+            }
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
