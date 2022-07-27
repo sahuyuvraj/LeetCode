b676900fa1241f225c7826a638930dd1dca20404
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-27 12:09:57 +0530

    Time: 3 ms (69.98%), Space: 9.2 MB (82.83%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -10,29 +10,19 @@
  * };
  */
 class Solution {
+public:
     int ans=0;
-    void dfs(TreeNode*root,int val){
+    
+    void sum(TreeNode*root,int val){
         if(!root)return;
         val*=10;
         val+=root->val;
         if(!root->left and !root->right)ans+=val;
-        dfs(root->left,val);
-        dfs(root->right,val);
+        sum(root->left,val);
+        sum(root->right,val);
     }
-public:
-    // void solve(TreeNode*root,int &sum,string s){
-    //     if(!root)return;
-    //     if(!root->left and !root->right)sum+=stoi(s);
-    //     if(root->left)solve(root->left,sum,s+to_string(root->left->val));
-    //     if(root->right)solve(root->right,sum,s+to_string(root->right->val));
-    // }
-    
     int sumNumbers(TreeNode* root) {
-        if(!root)return ans;
-        dfs(root,0);
+        sum(root,0);
         return ans;
-        // int sum=0;
-        // solve(root,sum,to_string(root->val));
-        // return sum;
     }
 };
