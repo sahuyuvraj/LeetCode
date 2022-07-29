7a10a0ba7b346ca3364ebb067b2213c5c7c73d1d
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-30 00:39:58 +0530

    Time: 358 ms (40.42%), Space: 11.4 MB (25.41%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        bool prime[n+1];
-       // if(n==0 or n==1)return 0;
-        memset(prime,true,sizeof(prime));
+        bool primes[n+1];
+        memset(primes,true,sizeof(primes));
+        
+        // primes[0]=false;
+        // primes[1]=false;
         for(int i=2;i*i<n;i++){
-            if(prime[i]==true){
+            if(primes[i]){
                 for(int j=i*i;j<=n;j+=i){
-                    prime[j]=false;
+                    primes[j]=false;
                 }
             }
         }
-        
         int cnt=0;
-        for(int i=2;i<n;i++)
-            if(prime[i])cnt++;
+        for(int i=2;i<n;i++)if(primes[i])cnt++;
         return cnt;
     }
 };
