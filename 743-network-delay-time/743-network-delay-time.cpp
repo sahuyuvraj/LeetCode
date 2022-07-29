b7b081d47d3eb99c0ce80bb3f399a38fff451b7a
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-07-29 08:03:40 +0530

    Time: 129 ms (89.21%), Space: 39.9 MB (70.58%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,59 +1,34 @@
 class Solution {
-    
-    struct node{
-      int dst;
-        int wt;
-    };
-    
-    struct qnode{
-      int vertex;
-        int dst;
-    };
-    
 public:
+
+    vector<pair<int, int>> adj[101];
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        
-        unordered_map<int,vector<node*>>adj;
-        
-        for(int i=0;i<times.size();i++){
-            node*newnode=new node;
-            newnode->dst=times[i][1];
-            newnode->wt=times[i][2];
-            adj[times[i][0]].push_back(newnode);
-        }
-        
-        qnode *qn=new qnode;
-        qn->vertex=k;
-        qn->dst=0;
-        queue<qnode*>q;
-        q.push(qn);
-        
-        vector<int>distance(n+1,INT_MAX);
-        distance[k]=0;
-        int time=0;
-        
-        while(!q.empty()){
-            
-            qnode*curr=q.front();
-            q.pop();
-            
-            for(auto it:adj[curr->vertex]){
-               
-                qnode*temp_qn=new qnode;
-                temp_qn->vertex=it->dst;
-                temp_qn->dst=it->wt+curr->dst;
-                if(distance[temp_qn->vertex]>temp_qn->dst){
-                    q.push(temp_qn);
-                    distance[temp_qn->vertex]=it->wt+curr->dst;
+        for (auto &a : times)
+            adj[a[0]].push_back({a[1], a[2]});
+        int ans = 0;
+        vector<int> dist(n + 1, INT_MAX);
+        priority_queue<pair<int, int>> pq;
+        pq.push({0, k});
+        dist[k] = 0;
+        while (!pq.empty())
+        {
+            pair<int, int> p = pq.top();
+            pq.pop();
+            for (auto &a : adj[p.second])
+            {
+                if (dist[a.first] > dist[p.second] + a.second)
+                {
+                    dist[a.first] = dist[p.second] + a.second;
+                    pq.push({-dist[a.first], a.first});
                 }
             }
         }
-        
-        //find the max distance
-        for(int i=1;i<=n;i++){
-            if(distance[i]==INT_MAX)return -1;
-            time=max(time,distance[i]);
+        for (int i = 1; i <= n; i++)
+        {
+            if (dist[i] == INT_MAX)
+                return -1;
+            ans = max(ans, dist[i]);
         }
-        return time;
+        return ans;
     }
 };
