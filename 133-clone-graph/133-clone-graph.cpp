de6bc0e38e9d83c8acd20b90239a6912b81c94dd
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-08-05 11:10:49 +0530

    Time: 12 ms (37.52%), Space: 9 MB (34.55%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -22,36 +22,47 @@ public:
 class Solution {
 public:
     
-    void dfs(Node*curr,Node*node,vector<Node*>&vis){
-        vis[curr->val]=node;
+//     void dfs(Node*curr,Node*node,vector<Node*>&vis){
+//         vis[curr->val]=node;
         
-        for(auto ele:curr->neighbors){
-            if(!vis[ele->val]){
-                Node*newnode=new Node(ele->val);
-                (node->neighbors).push_back(newnode);
-                dfs(ele,newnode,vis);
-            }else{
-                (node->neighbors).push_back(vis[ele->val]);
-            }
-        }
-    }
+//         for(auto ele:curr->neighbors){
+//             if(!vis[ele->val]){
+//                 Node*newnode=new Node(ele->val);
+//                 (node->neighbors).push_back(newnode);
+//                 dfs(ele,newnode,vis);
+//             }else{
+//                 (node->neighbors).push_back(vis[ele->val]);
+//             }
+//         }
+//     }
     
-    Node* cloneGraph(Node* node) {
-        if(node==NULL)return NULL;
+//     Node* cloneGraph(Node* node) {
+//         if(node==NULL)return NULL;
         
-        vector<Node*>vis(1000,NULL);
-        Node*copy=new Node(node->val);
-        vis[node->val]=copy;
+//         vector<Node*>vis(1000,NULL);
+//         Node*copy=new Node(node->val);
+//         vis[node->val]=copy;
         
-        for(auto curr:node->neighbors){
-            if(!vis[curr->val]){
-                Node*newnode=new Node(curr->val);
-                (copy->neighbors).push_back(newnode);
-                dfs(curr,newnode,vis);
-            }else{
-                (copy->neighbors).push_back(vis[curr->val]);
-            }
-        }
-        return copy;
-    }
+//         for(auto curr:node->neighbors){
+//             if(!vis[curr->val]){
+//                 Node*newnode=new Node(curr->val);
+//                 (copy->neighbors).push_back(newnode);
+//                 dfs(curr,newnode,vis);
+//             }else{
+//                 (copy->neighbors).push_back(vis[curr->val]);
+//             }
+//         }
+//         return copy;
+//     }
+    
+    unordered_map<Node*,Node*> m;
+	Node* cloneGraph(Node* node) {
+		if(!node) return NULL;
+		if(m.count(node)!=0) return m[node];
+		Node* copy_node = new Node(node->val);
+		m[node]=copy_node;
+		for(auto c:node->neighbors)
+			m[node]->neighbors.push_back(cloneGraph(c));
+		return m[node];
+	}
 };
