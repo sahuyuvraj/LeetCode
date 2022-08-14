6bd68a6d25bf9a156443b1ba767576b0758e3353
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-08-14 15:30:53 +0530

    Time: 7 ms (65.65%), Space: 6.7 MB (32.10%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if(n==1)return 0;
+        if (n == 1) return 0;
+        vector<int>dp(n + 1);
         
+        dp[0]=0;
+        dp[1]=0;
         
-        int ans=n;
-        for(int i=2;i<n;i++){
-            if(n%i==0){
-                ans = minSteps(i)+n/i;
+        for (int num=2;num<=n;num++) {
+            if(num&1){
+                for(int j=num/2;j>=1;j--){
+                    if(num % j == 0){
+                        dp[num]=dp[j]+num/j;
+                        break;
+                    }
+                }
+            } else {
+                dp[num] = dp[num/2]+2;
             }
         }
-        return ans;
+        return dp[n];
     }
 };
+
