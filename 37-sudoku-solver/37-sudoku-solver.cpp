266384f3af55132365fdd3c4688bacf1e3bc0375
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-08-19 12:16:13 +0530

    Time: 18 ms (92.21%), Space: 6.4 MB (49.83%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -44,48 +44,32 @@ public:
         return false;
     }
      
-    bool doSolve(vector<vector<char>>& board) {
-        for(int i=0; i<9; i++)
-        {
-            for(int j=0; j<9; j++)
-            {
-                if(board[i][j] == '.')
-                {                
-                    for(char c='1'; c<='9'; c++)
-                    {
-                        if(isvalid(board, i, j, c))
-                        {
-                            board[i][j] = c;
-                            if(doSolve(board))
-                                return true;
-                            board[i][j] = '.';
-                        }
-                    }
-                    return false;
-                }
-            }
-        }
+//     bool doSolve(vector<vector<char>>& board) {
+//         for(int i=0; i<9; i++)
+//         {
+//             for(int j=0; j<9; j++)
+//             {
+//                 if(board[i][j] == '.')
+//                 {                
+//                     for(char c='1'; c<='9'; c++)
+//                     {
+//                         if(isvalid(board, i, j, c))
+//                         {
+//                             board[i][j] = c;
+//                             if(doSolve(board))
+//                                 return true;
+//                             board[i][j] = '.';
+//                         }
+//                     }
+//                     return false;
+//                 }
+//             }
+//         }
         
-        return true;
-    }
+//         return true;
+//     }
    
-    // bool solve(vector<vector<char>>& board){
-    //     for(int i=0;i<9;i++){
-    //         for(int j=0;j<9;j++){
-    //             if(board[i][j]=='.'){
-    //                 for(char c='1';c<='9';c++){
-    //                     if(isvalid(board,i,j,c)){
-    //                         board[i][j]=c;
-    //                         if(solve(board))return true;
-    //                         board[i][j]='.';
-    //                     }
-    //                 }
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
+
     
     void solveSudoku(vector<vector<char>>& board) {
         solve(board,0,0);
