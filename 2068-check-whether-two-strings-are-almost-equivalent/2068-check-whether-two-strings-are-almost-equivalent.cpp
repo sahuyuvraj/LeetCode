6163d4b461a70359c21bc2448cc7b170791c1d22
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-08-26 22:48:35 +0530

    Time: 4 ms (50.72%), Space: 6.5 MB (31.34%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     bool checkAlmostEquivalent(string word1, string word2) {
-        unordered_map<char,int>mp,mp2;
-        for(char c:word1)mp[c]++;
-        
-        for(char c:word2){
-            if(!mp[c])mp2[c]++;
-            mp[c]--;
-            if(mp[c]<-3 or mp2[c]>3)return false;
-        }
-        
-        for(auto x:mp)if(x.second>3)return false;
+        unordered_map<char,int>sk;
+        for(auto i:word1){sk[i]++;}
+        for(auto i:word2){sk[i]--;}
+        for(auto i:sk){if(i.second>3 or i.second<-3){return false;}}
         return true;
     }
 };
