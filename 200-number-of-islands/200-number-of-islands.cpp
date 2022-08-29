584df739e2b189b2ae25d5fd3e32585391fae00e
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-08-29 18:12:11 +0530

    Time: 69 ms (30.69%), Space: 12.4 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    void mark(int i,int j,vector<vector<char>>&grid,int row,int col){
+    void bfs(int i,int j,vector<vector<char>>&grid,int row,int col){
         if(i<0 or i>=row or j<0 or j>=col or grid[i][j]!='1')return;
         grid[i][j]='2';
-        mark(i,j+1,grid,row,col);
-        mark(i+1,j,grid,row,col);
-        mark(i,j-1,grid,row,col);
-        mark(i-1,j,grid,row,col);
+        bfs(i,j+1,grid,row,col);
+        bfs(i+1,j,grid,row,col);
+        bfs(i,j-1,grid,row,col);
+        bfs(i-1,j,grid,row,col);
     }
     int numIslands(vector<vector<char>>& grid) {
         int row=grid.size();
@@ -16,7 +16,7 @@ public:
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]=='1'){
-                    mark(i,j,grid,row,col);
+                    bfs(i,j,grid,row,col);
                     count++;
                 }
             }
