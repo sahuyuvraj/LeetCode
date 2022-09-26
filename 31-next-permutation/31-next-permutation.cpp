b1235db5d3112f7fcdc4ec74f8c7501dccb0bcdf
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-09-27 01:42:57 +0530

    Time: 4 ms (78.75%), Space: 12.2 MB (36.45%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-//         int ind=-1;
-//         for(int i=nums.size()-2;i>=0;i--){
-//             if(nums[i]<nums[i+1]){
-//                 ind=i;
-//                 break;
-//             }
-//         }
-//         if(ind==-1){
-//             reverse(nums.begin(),nums.end());
-//             return;
-//         }
-//         int i;
-//         for(i=nums.size()-1;i>=0;i--){
-//             if(nums[i]>nums[ind]){
-//                 swap(nums[i],nums[ind]);
-//                 break;
-//             }
-//         }
+        int ind=-1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int i;
+        for(i=nums.size()-1;i>=0;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
         
-//         sort(nums.begin()+ind+1,nums.end());
-        next_permutation(nums.begin(),nums.end());
+        sort(nums.begin()+ind+1,nums.end());
+       // next_permutation(nums.begin(),nums.end());
     }
 };
