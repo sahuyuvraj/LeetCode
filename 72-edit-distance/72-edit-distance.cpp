d8402d607b017fa34e091b2c8270e5600642dcac
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-08 00:11:40 +0530

    Time: 8 ms (93.37%), Space: 7.2 MB (84.74%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(string s1,string s2,int m,int n){
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<=n;i++)dp[0][i]=i;
+        for(int j=0;j<=m;j++)dp[j][0]=j;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1])dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+    int minDistance(string word1, string word2) {
+        return solve(word1,word2,word1.size(),word2.size());
+    }
+};
