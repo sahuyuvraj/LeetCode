289a1ae231a2c7fa5ea9141fe01aaa6355a85d1c
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-07 23:32:54 +0530

    Time: 15 ms (53.03%), Space: 6.5 MB (77.89%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -3,22 +3,26 @@ public:
     string countAndSay(int n) {
         if(n==1)return "1";
         if(n==2)return "11";
-       
+        
         string s="11";
-        for(int i=3;i<=n;i++){
-            string t="";
-            s+='#';
+      
+       
+        for(int j=3;j<=n;j++){
+         string t="";
+            s+="#";     
             int cnt=1;
-            for(int j=1;j<s.size();j++){
-                if(s[j]!=s[j-1]){
+            for(int i=1;i<s.size();i++){
+                if(s[i-1]!=s[i]){
                     t+=to_string(cnt);
-                    t+=s[j-1];
+                    t+=s[i-1];
                     cnt=1;
-                }else cnt++;
+                }else{
+                    cnt++;
+                }
             }
-            
             s=t;
         }
+        
         return s;
     }
 };
