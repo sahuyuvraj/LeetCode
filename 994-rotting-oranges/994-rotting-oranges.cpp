e34340f20d862f38702101acd3184555bca8ff23
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-08 15:03:05 +0530

    Time: 19 ms (23.19%), Space: 13 MB (80.83%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     
-    bool checkall(vector<vector<int>>&grid){
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1)return true;
-            }
-        }
+    int check(vector<vector<int>>& grid) {
+         for(int i=0;i<grid.size();i++){
+             for(int j=0;j<grid[0].size();j++){
+                 if(grid[i][j]==1)return true;
+             }
+         }
         return false;
     }
-    
-    
     int orangesRotting(vector<vector<int>>& grid) {
+        
         int r=grid.size();
         int c=grid[0].size();
         
@@ -21,48 +20,50 @@ public:
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-            }   
+            }
         }
         
-        int count=0;
-        //delimiter
+        //delemeter
         q.push({-1,-1});
+        int count=0;
         
         while(!q.empty()){
             int i=q.front().first;
             int j=q.front().second;
             q.pop();
             
-            if(i==-1 and j==-1 and !q.size()){
-                break;
-            }else if(i==-1 and j==-1){
+            if(i==-1 and j==-1 and q.empty())break;
+            else if(i==-1 and j==-1){
                 count++;
                 q.push({-1,-1});
             }else{
-                
-                //check upside
-                if((i-1)>=0 and grid[i-1][j]==1){
+                //upside
+                if(i-1>=0 and grid[i-1][j]==1){
                     grid[i-1][j]=2;
                     q.push({i-1,j});
                 }
-                //check downside
-                if((i+1)<r and grid[i+1][j]==1){
+                
+                //downside
+                if(i+1<r and grid[i+1][j]==1){
                     grid[i+1][j]=2;
                     q.push({i+1,j});
                 }
                 
-                 //check upside
-                if((j-1)>=0 and grid[i][j-1]==1){
+              //leftside
+                if(j-1>=0 and grid[i][j-1]==1){
                     grid[i][j-1]=2;
                     q.push({i,j-1});
                 }
-                //check upside
-                if((j+1)<c and grid[i][j+1]==1){
+                
+                //rightside
+                if(j+1<c and grid[i][j+1]==1){
                     grid[i][j+1]=2;
                     q.push({i,j+1});
                 }
             }
+            
         }
-        return checkall(grid)?-1:count;
+        
+        return check(grid)?-1:count;
     }
 };
