34e506703131c9e9373f51f59f686a6ee6564b1e
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-08 13:27:11 +0530

    Time: 45 ms (27.98%), Space: 6.9 MB (41.70%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
-        
+        int ans=0;
         int n=s.size();
         int lt=0,rt=n-1;
         
-        int ans=0;
-        
         while(lt<rt){
-            
             int l=lt,r=rt;
+            
             while(s[l]!=s[r])r--;
             
             if(l==r){
@@ -27,6 +25,7 @@ public:
             
             lt++;rt--;
         }
+        
         return ans;
     }
 };
