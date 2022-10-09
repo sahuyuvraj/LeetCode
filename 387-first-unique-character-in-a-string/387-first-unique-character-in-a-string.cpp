54873ef5ec550b8a3d1b984d411138a66e970735
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-09 19:25:58 +0530

    Time: 47 ms (61.36%), Space: 10.6 MB (90.17%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-   for(int i = 0; i < s.length(); i++){
-        if(s.find_last_of(s[i]) == i && s.find(s[i]) == i) return i;
-    }
-    return -1;
+        for(int i=0;i<s.size();i++){
+            if(s.find_first_of(s[i])==i and s.find_last_of(s[i])==i)return i;
+        }
+        return -1;
     }
 };
