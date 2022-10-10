a774e6aa2f16dcb5a4f65cf4622054cf18e3be9b
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-11 00:09:54 +0530

    Time: 454 ms (95.11%), Space: 89.8 MB (81.61%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -9,10 +9,11 @@ public:
         sort(intervals.begin(),intervals.end(),cmp);
         int count=0;
         vector<int>res=intervals[0];
-        for(int i=1;i<intervals.size();i++){
+        
+        for(int i=1;i<intervals.size();i++)
             if(res[1]>intervals[i][0])count++;
             else res=intervals[i];
-        }
+        
         return count;
     }
 };
