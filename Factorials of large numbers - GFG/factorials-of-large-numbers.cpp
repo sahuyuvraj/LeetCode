e1d16fe28ddf005f4a6a95f5ccb02e93f5ec51bb
Yuvraj Sahu <75850180+sahuyuvraj@users.noreply.github.com>
2022-10-14 20:12:02 +0530

    Added solution - LeetHub

--- Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
+++ Factorials of large numbers - GFG/factorials-of-large-numbers.cpp
@@ -1,62 +1,63 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
+
 class Node{
     public:
-  int data;
-  Node*prev;
-  Node(int val){
-      data=val;
-      prev=NULL;
-  }
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
 };
-
-void mul(Node*tail,int i){
-    Node*curr=tail,*prev=tail;
-    
-    int carry=0;
-    
-    while(curr!=NULL){
-        int data=curr->data*i+carry;
-        curr->data=data%10;
-        carry=data/10;
-        prev=curr;
-        curr=curr->prev;
-    }
-    
-    while(carry!=0){
-        prev->prev=new Node(carry%10);
-        carry=carry/10;
-        prev=prev->prev;
-    }
-}
-
-void place(Node* tail,vector<int>&ans){
-    if(!tail)return;
-    place(tail->prev,ans);
-    ans.push_back(tail->data);
-}
 
 class Solution {
 public:
+   void insert(Node*head,int i){
+       Node*curr=head,*prev=NULL;
+       int carry=0;
+       while(curr){
+           int val=(curr->data*i)+carry;
+           curr->data=val%10;
+           carry=val/10;
+           prev=curr;
+           curr=curr->next;
+       }
+       
+        while(carry!=0){
+           prev->next=new Node(carry%10);
+           carry=carry/10;
+           prev=prev->next;
+       }
+       
+   }
+   
+   
+   void place(Node*head,vector<int>&ans){
+       while(head){
+           ans.push_back(head->data);
+           head=head->next;
+       }
+       reverse(ans.begin(),ans.end());
+   }
     vector<int> factorial(int N){
         // code here
-        vector<int>ans;
-        Node*tail=new Node(1);
+        Node*head=new Node(1);
         for(int i=2;i<=N;i++){
-            mul(tail,i);
+            insert(head,i);    
         }
-        
-        place(tail,ans);
+        vector<int>ans;
+        place(head,ans);
         return ans;
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -72,4 +73,5 @@ int main() {
         cout << endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
